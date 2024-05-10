f3bc7ab605ea61926f8944cfd9b874b4974cf6e5
Anger, S. (Sebastian) <sebastian.anger@ing.de>
2024-05-10 11:25:58 +0000

    feat: main.py load datasets

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Workshop Machine Learning 2
+
+## Datasets
+https://pytorch.org/vision/stable/datasets.html
--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import torch
+import torch.utils
+import torch.utils.data
+import torchvision
+import torchvision.transforms as transforms
+
+
 def main():
-    print("Hello World")
+  device = (
+      "cuda"
+      if torch.cuda.is_available()
+      else "mps"
+      if torch.backends.mps.is_available()
+      else "cpu"
+  )
+  print(f"Using {device} device")
+
+  train_dataset = torchvision.datasets.MNIST(root="./data", train=True, transform=transforms.ToTensor(), download=True)
+  test_dataset = torchvision.datasets.MNIST(root="./data", train=False, transform=transforms.ToTensor(), download=False)
+
 
 if __name__ == "__main__":
     main()
